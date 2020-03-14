2d6c48587f2285da8afb20087d0073c9655878c5
Vasyl Vasylyk <vasiliyworkplace@gmail.com>
2020-03-14 20:27:37 +0100

    summ regions

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -16,10 +16,39 @@ export class App extends React.Component {
         })
         .then(data => data.json())
         .then(res => {
-            const stats = res.data.covid19Stats;
-            stats.forEach(item => item.actual = item.confirmed - item.deaths - item.recovered);
-            this.setState({ stats });
+            const statsObject = {};
+            const statsArray = [];
+            const covidList = res.data.covid19Stats;
+            covidList.forEach(item => item.actual = item.confirmed - item.deaths - item.recovered);
+
+            covidList.forEach(item => {
+                if (statsObject[item.country]) {
+                    statsObject[item.country] = {
+                        confirmed: statsObject[item.country].confirmed + item.confirmed,
+                        deaths: statsObject[item.country].deaths + item.deaths,
+                        recovered: statsObject[item.country].recovered + item.recovered,
+                        lastUpdate: this.getLastUpdate(statsObject[item.country].lastUpdate, item.lastUpdate),
+                        actual: statsObject[item.country].actual + item.confirmed - item.deaths - item.recovered,
+                    }
+                } else {
+                    statsObject[item.country] = {
+                        confirmed: item.confirmed,
+                        deaths: item.deaths,
+                        recovered: item.recovered,
+                        lastUpdate: item.lastUpdate,
+                        actual: item.confirmed - item.deaths - item.recovered,
+                    }
+                }
+            })
+
+            Object.keys(statsObject).forEach(item => statsArray.push({ ...statsObject[item], ...{ country: item }}));
+
+            this.setState({ stats: statsArray });
         });
+    }
+
+    getLastUpdate(storedDate, newDate) {
+        return new Date(storedDate) < new Date(newDate) ? newDate : storedDate;
     }
 
     render() {
